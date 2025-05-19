4b5639b6b597b8b2b9d32f8aa46b92c8e2bc3ad2
Piyush Singhal <aniketsinghal4@gmail.com>
2025-05-19 11:54:15 +0530

    Updated something

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = process.env.PORT
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  console.log("Hello server");
+  res.send("Hello World!");
+});
 
-app.get('/piyush', (req, res) => {
-    res.send("Hello User");
-})
+app.get("/piyush", (req, res) => {
+  res.send("Hello User");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
-app.get('/hello', (req, res) => {
-    res.send("Hello there, lets play a game")
-})
-
+app.get("/hello", (req, res) => {
+  res.send("Hello there, lets play a game");
+});
